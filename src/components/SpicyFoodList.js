fdc2f0213ee028bc3c094c71ece2dc1cf7ef2c01
ogola5 <ogolaevance5@gmail.com>
2023-02-01 11:21:34 +0300

    revision

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,40 +2,38 @@ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
-
+  const[foods,setFoods]=useState(spicyFoods)
+  
   function handleAddFood() {
-    const newFood = getNewRandomSpicyFood();
-    const newFoodArray = [...foods,newFood]
+    const newFood = getNewRandomSpicyFood()
+    const newFoodArray=[...foods,newFood]
     setFoods(newFoodArray)
   }
   function handleClick(id){
-
-     const newFoodArray = foods.map((food)=>{
-      if (food.id ==id){
+    const newFoodArray = foods.map((food)=>{
+      if (food.id === id){
         return{
           ...food,
-          heatLevel:food.heatLevel +1
+          heatLevel:food.heatLevel + 1
         };
       }else{
         return food
       }
-     })
-     setFoods(newFoodArray)
+    })
+    setFoods(newFoodArray)
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id} onClick={() =>handleClick(food.id)}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+  const foodList = foods.map((food)=>(
+    <li key={food.id} onClick={()=>handleClick(food.id)}>
+      {food.name} | Heat: {food.heatLevel} | Cusine: {food.cuisine}
     </li>
   ));
-
-  return (
+  return(
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
+      <button onClick={handleAddFood}>Add new Food</button>
       <ul>{foodList}</ul>
     </div>
-  );
+  )
 }
 
 export default SpicyFoodList;
